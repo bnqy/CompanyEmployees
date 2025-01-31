faf6cc1f7b961f3626bbc17f42b46f68385dbf3f
bnqy <obbonobo@gmail.com>
2025-01-31 23:50:35 +0600

    Project configration.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace CompanyEmployees.Extensions;
+
+public static class ServiceExtensions
+{
+	// CORS - to restrict or give rights to apps from different domains
+	public static void ConfigCors(this IServiceCollection services)
+	{
+		services.AddCors(opts =>
+		{
+			opts.AddPolicy("CorsPolicy", builder =>
+			{
+				builder.AllowAnyOrigin() // WithOrigins("https://example.com") for concerete origin
+				.AllowAnyMethod() // WithMethods("POST", "GET") for specific methods
+				.AllowAnyHeader(); // WithHeaders("accept", "content-type") for specific headers.
+			});
+		});
+	}
+
+	// To host on IIS
+	public static void ConfigIISIntegration(this IServiceCollection services) => 
+		services.Configure<IISOptions>(opts =>
+		{
+
+		});
+
+}
