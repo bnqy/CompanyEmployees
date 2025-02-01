22add415b8c5687f200a65cb86387001d87d10cd
bnqy <obbonobo@gmail.com>
2025-02-01 20:59:00 +0600

    Repository pattern logic.

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,54 @@
+﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+	public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+	{
+		protected RepositoryContext repositoryContext;
+
+		public RepositoryBase(RepositoryContext repositoryContext)
+		{
+			this.repositoryContext = repositoryContext;
+		}
+
+		public void Create(T entity)
+		{
+			this.repositoryContext.Set<T>().Add(entity);
+		}
+
+		public void Delete(T entity)
+		{
+			this.repositoryContext.Set<T>().Remove(entity);
+		}
+
+		public IQueryable<T> FindAll(bool trackChanges)
+		{
+			return !trackChanges ?
+				this.repositoryContext.Set<T>()
+				.AsNoTracking() :
+				this.repositoryContext.Set<T>();
+		}
+
+		public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+		{
+			return !trackChanges ?
+				this.repositoryContext.Set<T>()
+				.Where(expression)
+				.AsNoTracking() :
+				this.repositoryContext.Set<T>()
+				.Where(expression);
+		}
+
+		public void Update(T entity)
+		{
+			this.repositoryContext.Set<T>().Update(entity);
+		}
+	}
+}
