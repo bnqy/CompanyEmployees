c4794b672aec5ca2bc4435675dfe4fefe8968395
bnqy <obbonobo@gmail.com>
2025-02-01 16:18:23 +0600

    Models & context class.

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// EF Core uses thede models(Entities) to map out DB
+namespace Entities.Models
+{
+	public class Company
+	{
+		[Column("CompanyId")]
+		public Guid Id { get; set; }
+
+		[Required(ErrorMessage = "Name is required.")]
+		[MaxLength(60, ErrorMessage = "Max length of Name is 60.")]
+		public string? Name { get; set; }
+
+		[Required(ErrorMessage = "Address is required.")]
+		[MaxLength(60, ErrorMessage = "Max length of Address is 60.")]
+		public string? Address { get; set; }
+
+		public string? Country { get; set; }
+
+		public ICollection<Employee>? Employees { get; set; } // navigational property: defines relationship between entities
+	}
+}
---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Project for the database context and repository implementation.
+namespace Repository
+{
+	public class RepositoryContext : DbContext
+	{
+		public RepositoryContext(DbContextOptions options)
+			: base(options)
+		{
+				
+		}
+
+		public DbSet<Company>? Companies { get; set; }
+
+		public DbSet<Employee>? Employees { get; set; }
+	}
+}
