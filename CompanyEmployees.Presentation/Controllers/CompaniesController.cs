de5a381ab64de12b9d03a01d500028f253d8a4de
bnqy <obbonobo@gmail.com>
2025-02-06 22:25:47 +0600

    Get collection of companies.

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CompanyEmployees.Presentation.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
 using Shared.DataTransferObjects;
 using System;
@@ -38,7 +39,7 @@ namespace CompanyEmployees.Presentation.Controllers
 		}
 
 		[HttpGet("collection/({ids})", Name = "CompanyCollection")]
-		public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+		public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
 		{
 			var companies = this.serviceManager.CompanyService.GetByIds(ids, false);
 
